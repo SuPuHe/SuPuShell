446a08a81fd990bf5f8875a291c8f8160ecd9636
Vspaceman11 <pushkarov.vladyslav@gmai.com>
2025-08-15 18:00:41 +0200

    Add commets to executors

--- srcs/executor_main_2.c
+++ srcs/executor_main_2.c
@@ -6,20 +6,22 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 12:45:49 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/08/15 17:27:54 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/08/15 17:58:28 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * @brief Executes an external command in a child process.
+ * @brief Executes an external command in the child process.
  *
- * Sets default signal handlers, runs the external command, clears history,
- * frees all memory, and exits with code 127 if execve fails.
+ * Restores default signal handlers for SIGINT and SIGQUIT. Executes
+ * the external command using run_external_command(). Cleans up
+ * readline history and frees all tracked memory. Exits with status 127
+ * if execution fails.
  *
- * @param command Pointer to input command structure.
- * @param shell Pointer to shell structure.
+ * @param command Pointer to the t_input structure representing the command.
+ * @param shell Pointer to the shell structure.
  */
 void	execute_external_child(t_input *command, t_shell *shell)
 {
