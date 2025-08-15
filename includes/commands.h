b088b6ebc3b0d382aab3a269bb375d138235f580
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2025-08-15 16:58:38 +0200

    Split functions in commands. Rename this files

--- includes/commands.h
+++ includes/commands.h
@@ -6,24 +6,30 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 15:30:25 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/08/04 14:53:52 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/08/15 16:55:43 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMANDS_H
 # define COMMANDS_H
 
-//commands
+//commands_1
 void	do_pwd(void);
 void	do_cd(t_shell *shell, char **commands, t_env **env);
-void	do_export(t_shell *shell, char **argv, t_env **env);
-void	do_unset(char **argv, t_env **env);
+void	cd_logic(t_shell *shell, char **commands, t_env **env,
+			const char *home);
+bool	handle_parsed_export_arg(char *key, char *val, t_env **env);
+void	print_all_env_vars(t_env *env);
 
-//commands_second_part
+//commands_2
 void	print_error(char *argv);
 void	do_echo(char **argv);
 void	run_external_command(char **argv, t_env *env);
 void	do_env(t_env *env, char **argv);
+
+//commands_3
+void	do_export(t_shell *shell, char **argv, t_env **env);
+void	do_unset(char **argv, t_env **env);
 
 //commands_helpers
 void	goto_prev_dir(t_env **env);
