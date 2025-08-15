13507addd14f63fbe7531fefce1cf560f1cf0972
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2025-08-15 15:32:12 +0200

    Split functions in string_builder and fit to norm

--- includes/string_builder.h
+++ includes/string_builder.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   string_builder.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:26:21 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/08/12 16:44:12 by omizin           ###   ########.fr       */
+/*   Updated: 2025/08/15 15:16:57 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 
 # include "types.h"
 
+// string_builder.c
 t_string_builder	*sb_create(void);
 void				sb_append_char(t_string_builder *sb, char c);
 void				sb_append(t_string_builder *sb, const char *s, bool quotes);
-char				*sb_build_and_destroy(t_string_builder *sb);
 
+// string_builder_second_part.c
+char				*normalize_spaces(const char *s);
+char				*sb_build_and_destroy(t_string_builder *sb);
 #endif
